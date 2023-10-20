27231f5ef58226c84d72a7b5f42c5351c001f6c9
Ambar Gonzalez <49761975+Ambarpg80@users.noreply.github.com>
2023-10-20 13:24:04 -0400

    added picture and CSS to home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,13 +2,18 @@ import React from "react";
 
 
 function Home(){
-     
+    //  const styling ={position: 'absolute',
+    //  top: "45vh", 
+    //  right: 30,
+      
+    // }
     
   return(
-    <div>
-        
-        
-    </div>
+    <div >  <span className="home-font">LitQuest</span>
+    <img className="home-page" src="https://static.demilked.com/wp-content/uploads/2019/01/5c4ace00c34af-Creative-Libraries-129-5c389b3de737e__700.jpg" alt="Cerritos Library Entrance"></img>
+   
+    </div>    
+   
     )
 }
 

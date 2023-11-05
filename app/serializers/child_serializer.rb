8830672dbf59e3ecd041ac1881718e0fc7fc2110
Ambar Gonzalez <49761975+Ambarpg80@users.noreply.github.com>
2023-11-05 10:34:03 -0500

    updated serializers to remove overlapping attributes and add custom method for duplicates

--- app/serializers/child_serializer.rb
+++ app/serializers/child_serializer.rb
@@ -1,11 +1,13 @@
 class ChildSerializer < ActiveModel::Serializer
-  attributes :id, :parent_id, :name, :age, :image_url #attributes passed to other models
+  attributes :id, :parent_id, :name, :age, :image_url # ,:my_books   #attributes passed to other models
 
   has_one :user_profile
-  has_many :books, through: :reviews
+  has_many :books, through: :reviews do
+    self.object.books.uniq
+  end
 
   def age
     self.object.user_profile.age
   end 
-  
+ 
 end
--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,6 +1,6 @@
 class ReviewSerializer < ActiveModel::Serializer
   attributes :id, :child_id, :book_id, :summary, :rating
 
-belongs_to :child
-belongs_to :book
+# belongs_to :child
+# belongs_to :book
 end
